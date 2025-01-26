fdf4a767955678336fe5ceb53e758ad833f3fdba
Sean Moran <36861508+seanpatrickmoran@users.noreply.github.com>
2025-01-26 15:22:26 -0500

    Update CCCImageClasses.py

--- CCCImageClasses.py
+++ CCCImageClasses.py
@@ -44,7 +44,7 @@ class C3Image4:
                        "condition": kwargs.get("condition",""),
                        "coordinates":self.coordinates,
 #                       maybe we convert the numpyarr into 4 channel images first?
-                       "numpyarr": bytes(_narr.flatten()),
+                       "numpyarr": _narr.flatten().tobytes(),
                        # "numpyarr": base64.b64encode(_narr.flatten().tobytes()).decode('utf-8'),
                        "viewing_vmax": viewing_vmax,
                        "dimensions": dimensions,
